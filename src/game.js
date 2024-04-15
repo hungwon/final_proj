d1ac5ab0ba4314bd7d685959a44cf8d3d72de2ef
Nithurhan Carthikeyan <nithuc25@gmail.com>
2024-04-14 23:15:15 -0700

    Added code to choose random  image and provide textbox, not tested yet

---
+++ src/game.js
@@ -0,0 +1,50 @@
+//game logic, return image path and restaurant name that corresponds to the image
+
+const restaurantNames = [
+    "Blondies",
+    "Dumping Kitchen",
+    "D'Yar",
+    "IB's",
+    "Kimchi Garden",
+    "Koja Kitchen",
+    "Murraci",
+    "Poke Parlor",
+    "Super Duper",
+    "T-Toust"
+  ];
+
+  //Is the path correct?
+const imagePaths = [
+    "img/blondies_food.PNG",
+    "img/dumpling_food.PNG",
+    "public/img/dyar_food.PNG",
+    "public/img/ibs_food.PNG",
+    "public/img/kimchigarden_food.PNG",
+    "public/img/koja_food.PNG",
+    "public/img/murraci_food.PNG",
+    "public/img/pokeparlor_food.PNG",
+    "public/img/superduper_food.PNG",
+    "public/img/ttoust_food.PNG"
+  ];
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function chooseImage(){
+    const imageRestaurantMapping = {};
+    for (let i = 0; i < imagePaths.length; i++) {
+        imageRestaurantMapping[imagePaths[i]] = restaurantNames[i];
+    }
+
+    const randomNumber = getRandomInt(0, 9);
+    const randomImagePath = imagePaths[randomIndex]; 
+    const imageContainer = document.getElementById("imageContainer");
+    imageContainer.src = randomImagePath;
+    const corName  = imageRestaurantMapping[randomImagePath];
+
+    //textbox
+    document.getElementById("guessInput").value = "";
+    imageContainer.setAttribute("data-restaurant", restaurantName);
+  }
+
